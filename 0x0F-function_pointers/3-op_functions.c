95d06c4a47233d8d8a601eafbb78a3d2155b7254
Vivian <okekevivian30@gmail.com>
2022-10-12 09:33:12 -0700

    Tasks3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - addition operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
