3834a00cb270d0a3b44a46f9c9c0b1be98de5a4a
Vivian <okekevivian30@gmail.com>
2022-10-12 09:43:17 -0700

    Tasks3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,8 +8,8 @@
  * function
  * @s: operator passed as argument to the program
  *
- * Return: a pointer to the function that corresponds to the operator 
- * given as a parameter
+ * Return: a pointer to the function that corresponds to the operator give
+ * as a parameter
  */
 int (*get_op_func(char *s))(int, int)
 {
